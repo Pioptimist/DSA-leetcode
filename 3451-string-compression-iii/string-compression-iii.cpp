d0ce514d382c0af1115148e18b92efb7a1e49786
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2025-07-29 14:52:41 +0530

    Time: 49 ms (36.04%) | Memory: 29.8 MB (9.60%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+        int n = word.size();
+        int i = 0;
+
+        while (i < n) {
+            char c = word[i];
+            int count = 0;
+
+            // count up to 9 of same char
+            while (i < n && word[i] == c && count < 9) {
+                count++;
+                i++;
+            }
+
+            comp += to_string(count) + c;
+        }
+
+        return comp;
+    }
+};
