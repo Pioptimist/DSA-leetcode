0c334bbe903bbc377dcd4c3f8a95904b18f53d04
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2025-09-09 19:36:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (29.26%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long num = x;
-        for(int i = 1;i<n;i++){
+    long long ans = x;
+    long long k = (long long)n - 1;   
+    int bit = 0;           
 
-            num = (num+1) | x;
+    for (int i = 0; i < 63; i++) {  
+        if (((long long)x >> i) & 1) {
+            continue; // bit already fixed in x so we dont change this
         }
-        return num;
+        if ((k >> bit) & 1) {
+            ans |= (1LL << i);  
+        }
+        bit++;
     }
+    return ans;
+}
+
 };
