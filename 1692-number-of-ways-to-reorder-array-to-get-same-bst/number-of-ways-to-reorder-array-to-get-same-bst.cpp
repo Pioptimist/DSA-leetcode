904771ba4b6db1c06c0d9b363f7afc01562ca677
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2025-09-17 01:40:34 +0530

    Time: 41 ms (84.22%) | Memory: 41.8 MB (77.81%) - LeetSync

--- 1692-number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ 1692-number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -1,52 +1,51 @@
 class Solution {
 public:
     typedef long long ll;
-    ll MOD = 1e9 + 7;
-    
-    vector<vector<ll>> PT;
-    
+    const ll MOD = 1e9 + 7;
+    vector<ll> fact, invfact;
+
+    ll modpow(ll a, ll b) {
+        ll res = 1;
+        while (b > 0) {
+            if (b & 1) res = (res * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    ll nCr(int n, int r) {
+        if (r < 0 || r > n) return 0;
+        return fact[n] * invfact[r] % MOD * invfact[n-r] % MOD;
+    }
+
     int solve(vector<int>& nums) {
         int m = nums.size();
-        
-        if(m < 3)
-            return 1;
-        
-        vector<int> left, right;   //left and right of root
-        
+        if (m < 3) return 1;
+
+        vector<int> left, right;
         int root = nums[0];
-        for(int i = 1; i < m; i++) {
-            
-            if(nums[i] < root) {
-                left.push_back(nums[i]);
-            } else {
-                right.push_back(nums[i]);
-            }
-            
+        for (int i = 1; i < m; i++) {
+            if (nums[i] < root) left.push_back(nums[i]);
+            else right.push_back(nums[i]);
         }
-        
-        ll leftways  = solve(left)  % MOD;
-        ll rightways = solve(right) % MOD;
-        
-        return (((leftways * rightways)%MOD) * PT[m-1][left.size()]) % MOD;
-        
+
+        ll leftways  = solve(left);
+        ll rightways = solve(right);
+
+        return (((leftways * rightways) % MOD) * nCr(m-1, left.size())) % MOD;
     }
-    
+
     int numOfWays(vector<int>& nums) {
         int n = nums.size();
-        
-        
-        PT.resize(n+1);
-        
-        for(int i = 0; i <= n; i++) {
-            
-            PT[i] = vector<long long>(i+1, 1);
-            
-            for(int j = 1; j < i; j++) {
-                PT[i][j] = (PT[i-1][j-1] + PT[i-1][j]) % MOD;
-            }
-        }
-        
-        
-        return (solve(nums)-1) % MOD;
+        fact.resize(n+1);
+        invfact.resize(n+1);
+
+        fact[0] = 1;
+        for (int i = 1; i <= n; i++) fact[i] = (fact[i-1] * i) % MOD;
+        invfact[n] = modpow(fact[n], MOD-2);
+        for (int i = n-1; i >= 0; i--) invfact[i] = (invfact[i+1] * (i+1)) % MOD;
+
+        return (solve(nums) - 1 + MOD) % MOD;
     }
 };
