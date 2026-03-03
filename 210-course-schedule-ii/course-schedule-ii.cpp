f63ec9a6010fd26482d27ac5887d812ab038f2ab
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-03-04 01:28:46 +0530

    Time: 11 ms (13.31%) | Memory: 18.4 MB (58.02%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> res;
+    bool dfs(int node,
+             vector<vector<int>>& adj,
+             vector<int>& vis,
+             vector<int>& path) {
+
+        vis[node] = 1;
+        path[node] = 1;
+
+        for(int nei : adj[node]) {
+            if(!vis[nei]) {
+                if(dfs(nei, adj, vis, path))
+                    return true;   
+            }
+            else if(path[nei]) {
+                return true;       
+            }
+        }
+
+        path[node] = 0;           
+        res.push_back(node);      
+        return false;
+    }
+
+    vector<int> findOrder(int numCourses,
+                          vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(numCourses);
+
+    
+        for(auto &p : prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
+            adj[prereq].push_back(course);  
+        }
+
+        vector<int> vis(numCourses, 0);
+        vector<int> path(numCourses, 0);
+
+        for(int i = 0; i < numCourses; i++) {
+            if(!vis[i]) {
+                if(dfs(i, adj, vis, path))
+                    return {};     
+            }
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
