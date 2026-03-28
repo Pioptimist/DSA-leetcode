5ecf98845e4f54ff07e752bb18a11f69dac592b4
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-03-28 20:20:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (16.48%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
+        int n = s1.size();
+        int m = s2.size();
+
+        // base cases
+        if(i == n) return m - j;   // insert remaining of s2
+        if(j == m) return n - i;   // delete remaining of s1
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        // match
+        if(s1[i] == s2[j]) {
+            return dp[i][j] = f(i+1, j+1, s1, s2, dp);
+        }
+
+        // mismatch → try all 3
+        int del = 1 + f(i+1, j, s1, s2, dp);       // delete
+        int ins = 1 + f(i, j+1, s1, s2, dp);       // insert
+        int rep = 1 + f(i+1, j+1, s1, s2, dp);     // replace
+
+        return dp[i][j] = min({del, ins, rep});
+    }
+
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(0, 0, word1, word2, dp);
+    }
+};
