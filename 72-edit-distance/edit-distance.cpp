919f7c03e7a95dfd533446f63a5fe5ca52878cdb
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-03-30 15:31:57 +0530

    Time: 10 ms (36.29%) | Memory: 14.5 MB (16.37%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -3,10 +3,8 @@ public:
     int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
         int n = s1.size();
         int m = s2.size();
-
-        // base cases
-        if(i == n) return m - j;   // insert remaining of s2
-        if(j == m) return n - i;   // delete remaining of s1
+        if(i == n) return m - j;   
+        if(j == m) return n - i;   
 
         if(dp[i][j] != -1) return dp[i][j];
 
@@ -15,10 +13,10 @@ public:
             return dp[i][j] = f(i+1, j+1, s1, s2, dp);
         }
 
-        // mismatch → try all 3
-        int del = 1 + f(i+1, j, s1, s2, dp);       // delete
-        int ins = 1 + f(i, j+1, s1, s2, dp);       // insert
-        int rep = 1 + f(i+1, j+1, s1, s2, dp);     // replace
+        
+        int del = 1 + f(i+1, j, s1, s2, dp);       
+        int ins = 1 + f(i, j+1, s1, s2, dp);       
+        int rep = 1 + f(i+1, j+1, s1, s2, dp);     
 
         return dp[i][j] = min({del, ins, rep});
     }
