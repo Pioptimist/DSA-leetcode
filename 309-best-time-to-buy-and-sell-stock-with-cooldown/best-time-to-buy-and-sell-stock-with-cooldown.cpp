28b524e3a593fd655d3937dd973f414db9bebe62
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-04-02 19:12:05 +0530

    Time: 3 ms (34.27%) | Memory: 16.2 MB (49.85%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -3,17 +3,22 @@ public:
     int maxProfit(vector<int>& p) {
         int n = p.size();
         vector<vector<int>> dp(n+2 , vector<int> (2 , 0));
-        for(int i = n-1 ; i>=0 ; i--){
-            for(int b = 0 ; b <=1 ; b++){
-                if(b==1){
-                    dp[i][b] = max(-p[i] + dp[i+1][0] , 
-                                   0 + dp[i+1][b]);
-                }
-                else{
-                    dp[i][b] = max(p[i] + dp[i+2][1] , 
-                                   0 + dp[i+1][0]);
-                }
-            }
+        // for(int i = n-1 ; i>=0 ; i--){
+        //     // for(int b = 0 ; b <=1 ; b++){
+        //     //     if(b==1){
+        //     //         dp[i][b] = max(-p[i] + dp[i+1][0] , 
+        //     //                        0 + dp[i+1][b]);
+        //     //     }
+        //     //     else{
+        //     //         dp[i][b] = max(p[i] + dp[i+2][1] , 
+        //     //                        0 + dp[i+1][0]);
+        //     //     }
+        //     // } dont need this inner loop
+            
+        // }
+        for(int i = n-1; i >= 0; i--){
+            dp[i][1] = max(-p[i] + dp[i+1][0], dp[i+1][1]);
+            dp[i][0] = max(p[i] + dp[i+2][1], dp[i+1][0]);
         }
 
         return dp[0][1];
