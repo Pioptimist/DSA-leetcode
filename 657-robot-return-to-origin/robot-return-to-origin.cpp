786ea7f7afd2f79492131c28d58c1b78f034b8a1
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-04-05 23:02:13 +0530

    Time: 15 ms (10.20%) | Memory: 10.3 MB (76.78%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        map<char , pair<int,int>> dir;
+        dir['U'] = {0,1};
+        dir['D'] = {0,-1};
+        dir['L'] = {-1,0};
+        dir['R'] = {1,0};
+        pair<int,int> posn = {0,0};
+        for(char c : moves){
+            posn.first += dir[c].first;
+            posn.second += dir[c].second;
+        }
+
+        return posn == pair<int,int>{0,0};
+    }
+};
