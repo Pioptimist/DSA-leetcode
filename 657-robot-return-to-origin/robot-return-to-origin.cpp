2a4ea4c58b073ad152fce4ad2b39ad6531122b8c
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-04-05 23:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (76.78%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -1,17 +1,32 @@
+// class Solution {
+// public:
+//     bool judgeCircle(string moves) {
+//         map<char , pair<int,int>> dir;
+//         dir['U'] = {0,1};
+//         dir['D'] = {0,-1};
+//         dir['L'] = {-1,0};
+//         dir['R'] = {1,0};
+//         pair<int,int> posn = {0,0};
+//         for(char c : moves){
+//             posn.first += dir[c].first;
+//             posn.second += dir[c].second;
+//         }
+
+//         return posn == pair<int,int>{0,0};
+//     }
+// };
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        map<char , pair<int,int>> dir;
-        dir['U'] = {0,1};
-        dir['D'] = {0,-1};
-        dir['L'] = {-1,0};
-        dir['R'] = {1,0};
-        pair<int,int> posn = {0,0};
+        int x = 0, y = 0;
+
         for(char c : moves){
-            posn.first += dir[c].first;
-            posn.second += dir[c].second;
+            if(c == 'U') y++;
+            else if(c == 'D') y--;
+            else if(c == 'L') x--;
+            else x++;
         }
 
-        return posn == pair<int,int>{0,0};
+        return x == 0 && y == 0;
     }
 };
