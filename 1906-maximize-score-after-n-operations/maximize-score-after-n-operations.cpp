e96e0e2f199b7ab0ce9d822dec5b84a5a976b3f1
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-04-11 17:31:50 +0530

    Time: 75 ms (44.01%) | Memory: 11.2 MB (87.26%) - LeetSync

--- 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.cpp
+++ 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.cpp
@@ -1,36 +1,73 @@
+// class Solution {
+// public:
+//     int f(vector<int>& nums, int n , vector<bool> vis, int op, 
+//     unordered_map<vector<bool> , int> &dp){
+//         int maxS = 0;
+//         if(op > n) return 0;
+//         if(dp.find(vis) != dp.end()) return dp[vis];
+
+//         for(int i = 0 ; i < n-1; i++){
+//             if(vis[i]== true) continue;
+
+//             for(int j = i+1;j<n;j++){
+//                 if(vis[j]== true) continue;
+
+//                 vis[i] = true;
+//                 vis[j] = true;
+               
+//                 int currS = op * __gcd(nums[i],nums[j]);
+//                 int rem = f(nums , n  ,vis , op+1 , dp);
+//                 maxS = max(maxS , currS + rem);
+
+//                 vis[i] = false;
+//                 vis[j] = false;
+
+//             }
+//         }
+//         return dp[vis] = maxS;
+//     }
+//     int maxScore(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<bool> vis(n , false);
+//         unordered_map<vector<bool> , int> dp;
+//         return f(nums , n ,vis , 1 , dp);
+        
+//     }
+// };
+
 class Solution {
 public:
-    int f(vector<int>& nums, int n , int i , int j, vector<bool> vis, int op, 
-    unordered_map<vector<bool> , int> &dp){
-        int maxS = 0;
+    int f(vector<int>& nums, int mask, int op, vector<int>& dp) {
+        int n = nums.size();
 
-        if(dp.find(vis) != dp.end()) return dp[vis];
-        
-        for(int i = 0 ; i < n-1; i++){
-            if(vis[i]== true) continue;
+        if(mask == (1 << n) - 1) return 0;
 
-            for(int j = i+1;j<n;j++){
-                if(vis[j]== true) continue;
+        if(dp[mask] != -1) return dp[mask];
 
-                vis[i] = true;
-                vis[j] = true;
-               
-                int currS = op * __gcd(nums[i],nums[j]);
-                int rem = f(nums , n , i , j+1 , vis , op+1 , dp);
-                maxS = max(maxS , currS + rem);
+        int maxScore = 0;
 
-                vis[i] = false;
-                vis[j] = false;
+        for(int i = 0; i < n; i++) {
+            if(mask & (1 << i)) continue;
 
+            for(int j = i + 1; j < n; j++) {
+                if(mask & (1 << j)) continue;
+
+                int newMask = mask | (1 << i) | (1 << j);
+
+                int curr = op * __gcd(nums[i], nums[j]);
+
+                int rem = f(nums, newMask, op + 1, dp);
+
+                maxScore = max(maxScore, curr + rem);
             }
         }
-        return dp[vis] = maxS;
+
+        return dp[mask] = maxScore;
     }
+
     int maxScore(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> vis(n , false);
-        unordered_map<vector<bool> , int> dp;
-        return f(nums , n , 0 , 0 , vis , 1 , dp);
-        
+        vector<int> dp(1 << n, -1);
+        return f(nums, 0, 1, dp);
     }
 };
