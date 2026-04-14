05ef39ba229621b403fadb7b1a57c3d376fbad46
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-04-14 21:45:55 +0530

    Time: 3 ms (46.39%) | Memory: 12 MB (15.36%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.cpp
+++ 956-number-of-music-playlists/number-of-music-playlists.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    int MOD = 1e9 + 7;
-    long long dp[101][101];
-
-    long long solve(int i, int j, int n, int k) {
-        // Base case
-        if (i == 0 && j == 0) return 1;
-        if (i == 0 || j == 0) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        long long ans = 0;
-
-        // Pick a NEW song ie a uniq song
-        ans += solve(i - 1, j - 1, n, k) * (n - (j - 1));
-
-        //  Repeat a song from the uniq songs we have picked till now ie j
-        if (j > k) {
-            ans += solve(i - 1, j, n, k) * (j - k);
+    int N;
+    int GOAL;
+    int K;
+    vector<vector<long>> dp;
+    const int MOD = 1e9+7;
+    
+    long solve(int i, int j) {
+        if(i == GOAL) {
+            if(j == N)
+                return 1;
+            
+            return 0;
         }
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        long result = 0;
+        
+        //Option-1 (Play a unique song)
+        if(j < N)
+            result += (N-j) * solve(i+1, j+1);
+        
+        //Option-2 (Replay a song)
+        if(j > K)
+            result += (j - K) * solve(i+1, j);
+        
+        
+        return dp[i][j] = result % MOD;
 
-        return dp[i][j] = ans % MOD;
     }
-
+    
     int numMusicPlaylists(int n, int goal, int k) {
-        memset(dp, -1, sizeof(dp));
-        return solve(goal, n, n, k);
+        N    = n;
+        GOAL = goal;
+        K    = k;
+        dp.assign(goal + 1, vector<long>(n + 1 , -1));
+        return solve(0, 0); 
     }
 };
