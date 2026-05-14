ce0d567ae731198afa734cdd84c184331ff31443
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-05-14 22:32:37 +0530

    Time: 2 ms (49.58%) | Memory: 16.4 MB (31.78%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,30 +12,64 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        map<int,map<int,multiset<int>>> mp;
+        queue<pair<TreeNode* , pair<int , int>>> q;  //node , x , lvl
+        map<int , map<int , multiset<int>>> mp;
+        vector<vector<int>> res;
+        if(root == nullptr) return res;
 
-        q.push({root,{0,0}});
+        q.push({root , {0 , 0}});
         while(!q.empty()){
             auto p = q.front();
             q.pop();
-            TreeNode* node = p.first;
+            TreeNode* node = p.first ; 
             int x = p.second.first;
-            int y = p.second.second;
-            if(node->left) q.push({node->left,{x-1,y+1}});
-            if(node->right) q.push({node->right,{x+1,y+1}});
-            mp[x][y].insert(node->val); 
+            int lvl = p.second.second;
+            if(node->left){
+                q.push({node->left , {x - 1, lvl + 1}});
+            }
+            if(node->right){
+                q.push({node->right , {x + 1, lvl + 1}});
+            }
+
+            mp[x][lvl].insert(node->val);
+
         }
 
-        vector<vector<int>> res;
         for(auto p : mp){
             vector<int> col;
-            for(auto q : p.second){
-                col.insert(col.end(), q.second.begin(), q.second.end());
- 
+            for(auto k : p.second){
+                col.insert(col.end() , k.second.begin(), k.second.end());
             }
             res.push_back(col);
         }
+
         return res;
     }
 };
+
+// queue<pair<TreeNode*,pair<int,int>>> q;  //Q({node , {x , lvl}})
+//         map<int,map<int,multiset<int>>> mp; //MP({x -> {lvl -> {nodes at x , lvl}}})
+
+//         q.push({root,{0,0}});
+//         // simple bfs
+//         while(!q.empty()){
+//             auto p = q.front();
+//             q.pop();
+//             TreeNode* node = p.first;
+//             int x = p.second.first;
+//             int lvl = p.second.second;
+//             if(node->left) q.push({node->left,{x-1,lvl+1}});
+//             if(node->right) q.push({node->right,{x+1,lvl+1}});
+//             mp[x][lvl].insert(node->val); //multiset mein insert
+//         }
+
+//         vector<vector<int>> res;
+//         for(auto p : mp){
+//             vector<int> col;
+//             for(auto q : p.second){
+//                 col.insert(col.end(), q.second.begin(), q.second.end());
+ 
+//             }
+//             res.push_back(col);
+//         }
+//         return res;
