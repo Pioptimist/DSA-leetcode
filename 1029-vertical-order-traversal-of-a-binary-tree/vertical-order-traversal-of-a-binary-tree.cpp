aa806ad183b32d47babbd6126105b4005e673669
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-06-23 02:59:48 +0530

    Time: 7 ms (5.21%) | Memory: 16.3 MB (44.68%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,33 +12,37 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode* , pair<int , int>>> q;  //node , x , lvl
-        map<int , map<int , multiset<int>>> mp;  // x , lvl , nodes val in that x,lvl in multiset bcz allows duplicates and sorted in cases two or more nodes share same (x,lvl)
+        queue<pair<TreeNode* , pair<int , int>>> q;
+        map<int , map<int , multiset<int>>> mp;
         vector<vector<int>> res;
         if(root == nullptr) return res;
 
         q.push({root , {0 , 0}});
-        while(!q.empty()){
-            auto p = q.front();
-            q.pop();
-            TreeNode* node = p.first ; 
-            int x = p.second.first;
-            int lvl = p.second.second;
-            if(node->left){
-                q.push({node->left , {x - 1, lvl + 1}});
-            }
-            if(node->right){
-                q.push({node->right , {x + 1, lvl + 1}});
-            }
 
-            mp[x][lvl].insert(node->val);// using insert bcz its a set
+        while(!q.empty()){
+    
+            int n = q.size();
+
+            for(int i = 0  ; i < n ; i++){
+                auto p = q.front();
+                TreeNode* node = p.first;
+                int x = p.second.first;
+                int lvl = p.second.second;
+
+                q.pop();
+
+                if(node->left != NULL) q.push({node->left , { x - 1 , lvl + 1}});
+                if(node->right != NULL) q.push({node->right , { x + 1 , lvl + 1}});
+
+                mp[x][lvl].insert(node->val);
+            }
 
         }
-//now those nodes which has same x goes in col vector , also map keys are always sorted so thats a plus pt.
+
         for(auto p : mp){
             vector<int> col;
-            for(auto k : p.second){  //inner map ke keys y atq lvl traverse 
-                col.insert(col.end() , k.second.begin(), k.second.end());
+            for(auto k : p.second){
+                col.insert(col.end(), k.second.begin(), k.second.end());
             }
             res.push_back(col);
         }
