3e44d85ca79eea1c64e4f61294e1013fa1065be2
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-06-30 03:14:48 +0530

    Time: 32 ms (5.07%) | Memory: 17.4 MB (98.72%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
-    vector<int> res;
-    bool dfs(int node,vector<vector<int>>& adj,vector<int>& vis,vector<int>& path) {
-        vis[node] = 1;
-        path[node] = 1;
-        for(int nei : adj[node]) {
-            if(!vis[nei]) {
-                if(dfs(nei, adj, vis, path))
-                    return true;   
-            }
-            else if(path[nei]) {
-                return true;       
+   vector<int> ans;
+   // this is a directed graph since explicitly bol rkha hai kaunsa node kiska parent hai
+    bool detect(vector<int> &vis ,vector<int> &path, vector<vector<int>>& prerequisites , int src, int n){
+        vis[src] = 1;
+        path[src] = 1;
+        for(int i = 0; i< n; i++){ //src ko dhundo in prereq and then waha se dfs lgao
+        
+            if(prerequisites[i][1] == src ){
+                if(!vis[prerequisites[i][0]]){
+                    if(detect(vis, path , prerequisites , prerequisites[i][0] , n)) return true;
+                }
+                else if(path[prerequisites[i][0]]) return true;
             }
         }
-
-        path[node] = 0;           
-        res.push_back(node);      
+        path[src] = 0;
+        //we make path = 0 when we backtrck ie ki cycle toh nhi bna mtlb yeh safe hai , issi order ko store krlo , aur baad mein reverse krke ans
+        ans.push_back(src);
         return false;
     }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto &p : prerequisites) {
-            int course = p[0];
-            int prereq = p[1];
-            adj[prereq].push_back(course);  
-        }
-        vector<int> vis(numCourses, 0);
-        vector<int> path(numCourses, 0);
-        for(int i = 0; i < numCourses; i++) {
-            if(!vis[i]) {
-                if(dfs(i, adj, vis, path))
-                    return {};     
+        if(prerequisites.size() == 0) {
+            vector<int> res;
+            for(int i = 0 ; i < numCourses ; i++){
+                res.push_back(i);
             }
+            return res;
         }
-        reverse(res.begin(), res.end());
-        return res;
+        int n = prerequisites.size();
+      
+        
+        vector<int> vis(numCourses,0);
+        vector<int> path(numCourses,0);
+        for(int i = 0; i< numCourses ; i++ ){
+            if(!vis[i]){
+
+                if(detect(vis , path,prerequisites , i , n)) return {};
+            }
+            
+        }
+        reverse(ans.begin() , ans.end());
+
+        return ans;
+        
     }
 };
