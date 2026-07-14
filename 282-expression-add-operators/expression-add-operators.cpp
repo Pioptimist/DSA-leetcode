4c06e25365ca628b1c44054d8adc48a01fb7df29
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-07-14 17:32:42 +0530

    Time: 428 ms (30.55%) | Memory: 92.8 MB (70.43%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -47,8 +47,18 @@ public:
 
     vector<string> addOperators(string num, int target) {
 
-        solve(num, target, 0, "", 0, 0);
+        solve(num, target, 0, "", 0, 0); //num ,target , ind , expr , total value , prev operand
 
         return ans;
     }
 };
+
+//ques of simple recursion and backtracking where we keep selecting digits and apply opretors between them , but one thing which fucks this up is , the mult operator which makes us follow operator precedance for eg say we have num = "123", target = 6 , we call recursion we selected 1 and then call recursion for [2,3] and suppose we put + after so 1 + [2,3] , then we chose 2 so 1 + 2 and now we choose mult for eg 1 + 2 * 3 , if we didnt have prev then there was no way to distinguish whether we should do 3 * 3 which is 9 or do 1 + 6=7 which follows operator precedance
+
+//hence in prev var we send the prev operand so like for 1 + 2 , prev is 2 same for - like 1 - 2 , prev is 2 but for mult now suppose we got (1 + 2) ie value is 3 and we called next recursion with mult with prev being 2 obv now we know we need 1 + 2*3 not 3*3 so we do val - prev + prev*currnum => 3-2 + 2*3 = 7. so prev in this call becomes 2*3 or prev*currnum.
+
+//next up is leading zeros , take this number for eg 105 , we start the call from ind = 0 ie on 1 now we call the next call ie 1 +/*/- [05] , remember this , we can take 0 as a number itself but anything starting with 0 is not a valid number like 05 , 012 , 09 are not valid bcz 14 * 05 or 345-0123 doesnt make sense.
+//continuing the eg of 105 now we are the next call ie we take 0 as solo , the cond if (i > index && num[index] == '0') doesnt satisfies as index is 1 and i =1 too but in next trav ie 05 the cond gets true and we break out of the loop , we dont continue bcz as soon as ek bhi leading zero aa gya , usse bnne wale hr ek number invalid honge be that be 05 , 07 ,012,0123,02534 anything.
+
+//also one last thing the idea of conveting the string expression adn calc the val at base case to match with target might come to mind but that will fail tc so we evaluate the expression on the go .
+                
