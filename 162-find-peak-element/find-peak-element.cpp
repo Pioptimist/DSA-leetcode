4b39db952e60ccbb73ee4d4f153b507f4b54939a
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-07-15 19:56:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (40.24%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -9,12 +9,14 @@ public:
         int high = n-2;
         while(low<=high){
             int mid = (low+high)/2;
+
             if(nums[mid]>nums[mid+1] && nums[mid]> nums[mid-1]) return mid;
+
             else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]){
-                low = mid+1;
+                low = mid+1;   //inc side ie aage peak milega
             }
             else if(nums[mid]>nums[mid+1] && nums[mid]<nums[mid-1]){
-                high = mid-1;
+                high = mid-1;  //dec side so piche peak milega
             }
             else{
                 high=mid-1;
