38f4427b83c07cf90dd952504e2151d173176599
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-07-16 02:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (79.89%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -9,21 +9,29 @@ public:
         int lefthalf = (n1+n2+1)/2;
         int n = (n1+n2);
         while(low<=high){
-           int l1 = INT_MIN, l2 = INT_MIN;
+           int l1 = INT_MIN, l2 = INT_MIN;  //for easy comparisons if mid1 or mid2 chosen is 0
            int r1 = INT_MAX , r2 = INT_MAX;
+
+//remember here mid1 means number of elements picked in smallest arr which is arr1 and mid2 same for arr2 , so mids can hold value from 0 to n1/n2 hence the conditions to be safer.
            int m1 = (low+high)/2;
            int m2 = lefthalf - m1;
+
+//mid will always point to r1/r2
            if(m1<n1) r1 = nums1[m1];
            if(m2<n2) r2 = nums2[m2];
            if(m1-1>=0) l1 = nums1[m1-1];
            if(m2-1>=0) l2 = nums2[m2-1];
            if(l1<=r2 && l2<=r1){
+
             if(n%2==1) return (double)max(l1,l2);
+
             return (double)(max(l1,l2)+min(r1,r2))/2;
            }
+
            else if(l1>r2){
             high=m1-1;
            }
+           
            else{
             low = m1 +1;
            }
