b373f7e040b6861e6c861f478f62cf2e82b182c3
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-07-22 14:35:53 +0530

    Time: 27 ms (79.10%) | Memory: 24.4 MB (63.49%) - LeetSync

--- 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int n;
 
-    int f(int i, int prev,
-          vector<pair<int,int>> &players,
-          vector<vector<int>> &dp) {
+    int f(int i, int prev, vector<pair<int,int>> &players, vector<vector<int>> &dp) {
 
         if(i == n)
             return 0;
@@ -17,32 +15,42 @@ public:
 
         // Take current player
         int take = 0;
+        if(prev == -1 || players[prev].second <= players[i].second) {//take scores in an inc manner but also we can pick equal scores too 
 
-        if(prev == -1 ||
-           players[prev].second <= players[i].second) {
-
-            take = players[i].second +
-                   f(i + 1, i, players, dp);
+            take = players[i].second + f(i + 1, i, players, dp);
         }
 
         return dp[i][prev + 1] = max(skip, take);
     }
 
-    int bestTeamScore(vector<int>& scores,
-                      vector<int>& ages) {
+    int bestTeamScore(vector<int>& scores,vector<int>& ages) {
 
         n = scores.size();
-
         vector<pair<int,int>> players;
 
         for(int i = 0; i < n; i++)
             players.push_back({ages[i], scores[i]});
 
-        sort(players.begin(), players.end());
+        sort(players.begin(), players.end());  //sort by age bcz now we dont have to wrry about age now 
 
-        vector<vector<int>> dp(n,
-                               vector<int>(n + 1, -1));
+        // vector<vector<int>> dp(n,vector<int>(n + 1, -1));
+        // return f(0, -1, players, dp);
 
-        return f(0, -1, players, dp);
+        vector<int> dp(n); //maximum team score ending at player i
+
+        for(int i = 0; i < n; i++)
+            dp[i] = players[i].second;
+
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                
+                if(players[j].second <= players[i].second) {
+                    dp[i] = max(dp[i],dp[j] + players[i].second);
+                }
+            }
+        }
+
+        return *max_element(begin(dp) , end(dp));
     }
 };
+// [10 , 11 , 12 , 9 , 8 , 7 , 6, 5] in this eg after sorting scores by age , noctice , 10 ,11 , 12 is the inc subseq so we take them not 9,8,7,6,5 bcz its strictly said score of the older one has to be >= score of the younger , hence in our recrusion we take a inc subseq.
