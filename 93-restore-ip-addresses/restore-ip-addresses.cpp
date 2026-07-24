078eb6a86ffd241931437cefc96b466f96901f12
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-07-24 15:25:17 +0530

    Time: 7 ms (10.39%) | Memory: 15.9 MB (5.87%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -9,9 +9,10 @@ public:
     }
     int n;
     vector<string> ans;
+//here on every time a recursion call comes back , we need to undo , thats why  its prefereed to not use & with any variable that way we wont need to do any popping or removal as every recursion call will have its own copy
     void solve(string &s , int i , string curr , int parts){
         if(parts == 4 && i == n){
-
+//agr 4 parts hai and each part is 0 <= x <= 255 then its a valid ip
             curr.pop_back();
             ans.push_back(curr);
 
@@ -29,7 +30,7 @@ public:
     }
     vector<string> restoreIpAddresses(string& s) {
         n = s.size();
-
+        if(n > 12) return {};
         solve(s , 0 , "" , 0);
 
         return ans;
