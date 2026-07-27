4a352ff85f598c045f44a912ea7eabd32df0a181
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-07-28 02:59:01 +0530

    Time: 5 ms (85.42%) | Memory: 52.4 MB (79.37%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,51 +1,47 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph , vector<int> &vis, vector<int> &path , int n , int src){
-        vis[src] = 1;  //assume safe
-        path[src] = 1;
+//logic is simple , agr koi bhi node kisi loop ka part hai then it is not at all safe , those nodes which are not part of loop will be marked 1 , and the ones which are will be marked -1
+    bool dfs(vector<int> &vis , vector<int>& path ,int node, vector<vector<int>>& graph){
+        vis[node] = 1;
+        path[node] = 1;
 
-        for(int adj : graph[src]){
+        for(int adj : graph[node]){
             if(!vis[adj]){
-                if(dfs( graph , vis , path , n , adj)){
-                    vis[src] = -1;  //unsafe
+                if(dfs(vis , path , adj , graph)){    //curr node loop ka part hai
+                    vis[node] = -1;
                     return true;
                 }
             }
             else if(vis[adj] == -1){
-
-                vis[src] = -1;
+                vis[node] = -1;   //agr adj node , loop ka part hai then again we can say curr node bhi hai, this case is when a diff node is joining to a cycle
                 return true;
             }
-            if(path[adj]){
-                vis[src] = -1;  //unsafe
-                return true; 
+            else if(path[adj]){
+                vis[node] = -1;  //same node wapis same pe wapis mil gya
+                return true;
             }
             
         }
-        path[src] = 0;
+        path[node] = 0;
         return false;
+
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-    
+
+        vector<int> vis(n , 0);
+        vector<int> path(n , 0);
         vector<int> res;
-        vector<int> path(n,0);
-        vector<int> vis(n,0);
-
-        for(int i =0; i<n ; i++){
-
+        for(int i = 0 ; i < n ; i++){
             if(!vis[i]){
-                dfs(graph , vis ,path,  n , i);
+                dfs(vis , path , i , graph );
+            }    
 
-            }
         }
 
-        for(int i=0; i <n ; i++){
-            if(vis[i] == 1){
-                res.push_back(i);
-            }
+        for(int i = 0 ; i< n ; i++){
+            if(vis[i] == 1) res.push_back(i);
         }
-
         return res;
 
         
