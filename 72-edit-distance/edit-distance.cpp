c9c02b5575207ae2f52f33eb0d2c9a936d6d7002
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-08-13 21:02:06 +0530

    Time: 10 ms (33.62%) | Memory: 14.4 MB (14.72%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -3,20 +3,24 @@ public:
     int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
         int n = s1.size();
         int m = s2.size();
-        if(i == n) return m - j;   
-        if(j == m) return n - i;   
+
+        if(i == n) return m - j;   //means s1 empty ho chuka aur s2 is remaining ie empty string ko bache hue s2 ke string ke saath match ke liye number of operations needed => m-j insert operations , for eg turn "" to "ro" , we would obv need 2 insert op
+
+        if(j == m) return n - i;   //means s2 exhausted mtlb s1 rh gya and s2 is completely matched in s1 , for eg "hrose" "rose" , and this base case suggest we matched rose so what remains is "h" and "" , so to convert a string to empty string => delete the remining chars
 
         if(dp[i][j] != -1) return dp[i][j];
 
         // match
         if(s1[i] == s2[j]) {
-            return dp[i][j] = f(i+1, j+1, s1, s2, dp);
+            return dp[i][j] = f(i+1, j+1, s1, s2, dp); //match kr gya toh dont do nothing
         }
 
-        
-        int del = 1 + f(i+1, j, s1, s2, dp);       
-        int ins = 1 + f(i, j+1, s1, s2, dp);       
-        int rep = 1 + f(i+1, j+1, s1, s2, dp);     
+        //only when mismatches , try to do the bottom three operation
+        int del = 1 + f(i+1, j, s1, s2, dp);     //we deleted a char in s1 ie ki iss char ko delete krke aage dekho ki shyd jth char match ho jaye
+
+        int ins = 1 + f(i, j+1, s1, s2, dp);     //we inserted a char before ith char to match with jth char , so obv i will remain where it was for future matching
+
+        int rep = 1 + f(i+1, j+1, s1, s2, dp);   //char replacement so both chars matched and goes ahead  
 
         return dp[i][j] = min({del, ins, rep});
     }
