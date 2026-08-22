7b2650abe9eaf12b4aaf4e3a416f2feecbd80293
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-08-22 15:01:08 +0530

    Time: 12 ms (29.94%) | Memory: 37.7 MB (23.43%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     void dfs(int i, int j , vector<vector<int>>& grid, vector<vector<int>> &vis, int n , int m){
+        vis[i][j] = 1;
         int dir[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         for(int d = 0; d<4 ; d++){
             int newr = i + dir[d][0];
             int newc = j + dir[d][1];
             if(newr>=0 && newr<n && newc >=0 && newc<m && vis[newr][newc] == 0 && grid[newr][newc]==1){
-                vis[newr][newc] = 1;
+                
                 dfs(newr , newc , grid , vis , n, m);
             }
         }
@@ -20,7 +21,7 @@ public:
             for(int j =0; j< m; j++){
                 if(!vis[i][j] && (i == 0 || i == n-1 || j == 0 || j == m-1)){
                     if(grid[i][j] == 1){
-                        vis[i][j] = 1;
+                        
                         dfs(i,j , grid ,vis , n , m);
                     }
                 }
