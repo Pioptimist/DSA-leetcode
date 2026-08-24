4c3702f369a1ebfdb58968c44efe33fb068a9ca0
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-08-25 00:30:39 +0530

    Time: 3 ms (97.37%) | Memory: 22.5 MB (49.07%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -6,8 +6,7 @@ public:
         parent.resize(n);
         rank.resize(n,0);
 
-        for(int i=0;i<n;i++)
-            parent[i] = i;
+        for(int i=0;i<n;i++)  parent[i] = i;
     }
 
     int find(int x){
@@ -36,37 +35,37 @@ public:
 
 class Solution {
 public:
+//treat rows and cols as nodes 
     int removeStones(vector<vector<int>>& stones) {
-
         int maxr = 0;
         int maxc = 0;
 
         for(auto &it: stones){
-            maxr = max(maxr, it[0]);
+            maxr = max(maxr, it[0]); //find the number of rows here and cols
             maxc = max(maxc, it[1]);
         }
 
-        DSU ds(maxr + maxc + 2);
+        DSU ds(maxr + maxc + 2); //total number of nodes is r + c + 2
 
         unordered_map<int,int> mp;
 
         for(auto &it : stones){
             int noder = it[0];
-            int nodec = it[1] + maxr + 1;
+            int nodec = it[1] + maxr + 1;  //to represent col as node , for 0th col we do 0 + r + 1. for eg there are 4 rows , r = 3 , so node = 0,1,2,3 for row to represent 0th col , we do 0 + 3 + 1 ie 4,5,6 ...
 
             ds.unite(noder, nodec);
-
-            mp[noder] = 1;
+            
+            mp[noder] = 1;   //keep overwriting bcz at the end we need only the nodes used which will help us cnt the conn component
             mp[nodec] = 1;
         }
 
-        int cnt = 0;
+        int cnt = 0; //cnt ult parent to cnt number of conn components
 
         for(auto &it : mp){
-            if(ds.find(it.first) == it.first)
-                cnt++;
+            if(ds.find(it.first) == it.first) cnt++;
         }
 
         return stones.size() - cnt;
     }
 };
+//numbers of removal is total number of stones - number of connected components
