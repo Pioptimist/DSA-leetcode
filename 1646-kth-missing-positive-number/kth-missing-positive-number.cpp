60b747de6f559404f5d19da67be6962200a150a2
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-08-28 13:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (92.43%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -5,9 +5,12 @@ public:
         int n = arr.size();
         int high = n-1;
         while(low<=high){
+
             int mid = (low+high)/2;
-            int missing = arr[mid]-(mid+1);
-            if(missing<k){
+
+            int missing = arr[mid]-(mid+1);  //num of missing elements at this ind
+
+            if(missing < k){
                 low = mid +1;
             }
             // else if(missing == k) return arr[mid]-1; wont work dry run on this [2,3,4,9,10]
@@ -16,6 +19,8 @@ public:
             }
         }
         return high+1+k;
+
+        //the actual formula is nums[high] + more where mre is k - missing numbers at that index so we can say nums[h] + k -(nums[h] - h - 1) [the formula for missing number at index h] , on relaxing we get this high + 1 + k.
         
     }
 };
