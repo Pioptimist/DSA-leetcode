9fc4b610cf89cf91dda1d0a1bd381034f96495b6
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-01 19:12:30 +0530

    Time: 131 ms (56.40%) | Memory: 13.5 MB (87.09%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool f(vector<int> &nums , int t , int ind, vector<vector<int>> &dp){
-        if(t == 0) return dp[ind][t] = 1;
-        if(ind == 0) return dp[ind][t] = (nums[ind] == t);
+    // bool f(vector<int> &nums , int t , int ind, vector<vector<int>> &dp){
+    //     if(t == 0) return dp[ind][t] = 1;
+    //     if(ind == 0) return dp[ind][t] = (nums[ind] == t);
 
-        if(dp[ind][t] != -1) return dp[ind][t];
+    //     if(dp[ind][t] != -1) return dp[ind][t];
 
-        bool notT = f(nums , t , ind - 1, dp);
-        bool take = 0;
+    //     bool notT = f(nums , t , ind - 1, dp);
+    //     bool take = 0;
 
-        if(t >= nums[ind]){
-            take = f(nums , t - nums[ind] , ind - 1 , dp);
-        }
+    //     if(t >= nums[ind]){
+    //         take = f(nums , t - nums[ind] , ind - 1 , dp);
+    //     }
 
-        return dp[ind][t] = notT || take;
-    }
+    //     return dp[ind][t] = notT || take;
+    // }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int t = 0;
         for(int i =0;i< n; i++){
             t += nums[i];
         }
-        vector<vector<int>> dp(n+1 , vector<int>(t/2 + 1 , -1));
+        // vector<vector<int>> dp(n+1 , vector<int>(t/2 + 1 , -1));
         if(t % 2 != 0) return false;  //if sum is odd , cant divide in 2 halves
 
         // return f(nums , t/2 , n-1 , dp);
