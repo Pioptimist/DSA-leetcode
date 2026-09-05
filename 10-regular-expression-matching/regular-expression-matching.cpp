bf19da93d6b8f3d197aa030259119fa7b5e93844
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-05 15:07:47 +0530

    Time: 4 ms (24.76%) | Memory: 9.7 MB (16.88%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -4,43 +4,43 @@ public:
     int m;
     // the crux of this question is if at current j , j+1 is * , then we take j
     // and j+1 or x* as one single char and try matching
-    bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
-        if (i >= n && j >= m) {
-            return true;
-        }
-        // else if((i >= n && j < m) || (i<n && j>=m)){
-        //     return false;
-        // }
-        // the above commented base case which seems obv isnt true always bcz of
-        // cases like s="" p="a*b*" here i==n and j < m but this will still give
-        // a match since we can just skip a* and b* for an empty string match
-        else if (i < n && j >= m) {
-            return false;
-        } 
-        else if (i >= n && j < m) {
-            if (j + 1 < m && p[j + 1] == '*') {
-                return f(i, j + 2, s, p,dp); // skip the x* and check again if the remaining p can match an empty string or not
-            }
-            return false; // if no x* , def return false
-        }
-        if (dp[i][j] != -1)
-            return dp[i][j];
+    // bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+    //     if (i >= n && j >= m) {
+    //         return true;
+    //     }
+    //     // else if((i >= n && j < m) || (i<n && j>=m)){
+    //     //     return false;
+    //     // }
+    //     // the above commented base case which seems obv isnt true always bcz of
+    //     // cases like s="" p="a*b*" here i==n and j < m but this will still give
+    //     // a match since we can just skip a* and b* for an empty string match
+    //     else if (i < n && j >= m) {
+    //         return false;
+    //     } 
+    //     else if (i >= n && j < m) {
+    //         if (j + 1 < m && p[j + 1] == '*') {
+    //             return f(i, j + 2, s, p,dp); // skip the x* and check again if the remaining p can match an empty string or not
+    //         }
+    //         return false; // if no x* , def return false
+    //     }
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
 
-        if (j + 1 < m && p[j + 1] == '*') {
-            if (i < n && (s[i] == p[j] || p[j] == '.')) {
-                return dp[i][j] = f(i + 1, j, s, p, dp)  ||  f(i, j + 2, s, p,dp);// match the chars but since x* can have more occur of x , we dont move j pointer or we dont match at all and skip x* ie we took 0 occurences
-            }
+    //     if (j + 1 < m && p[j + 1] == '*') {
+    //         if (i < n && (s[i] == p[j] || p[j] == '.')) {
+    //             return dp[i][j] = f(i + 1, j, s, p, dp)  ||  f(i, j + 2, s, p,dp);// match the chars but since x* can have more occur of x , we dont move j pointer or we dont match at all and skip x* ie we took 0 occurences
+    //         }
             
-            // even if s[i] doesnt match with x* , there is a chance we can skip
-            // x* and match   s[i] ahead
-            return dp[i][j] = f(i, j + 2, s, p, dp);
-        } else {
-            if (i < n && (s[i] == p[j] || p[j] == '.')) {
-                return dp[i][j] = f(i + 1, j + 1, s, p, dp);
-            }
-            return false; // clear mismatch
-        }
-    }
+    //         // even if s[i] doesnt match with x* , there is a chance we can skip
+    //         // x* and match   s[i] ahead
+    //         return dp[i][j] = f(i, j + 2, s, p, dp);
+    //     } else {
+    //         if (i < n && (s[i] == p[j] || p[j] == '.')) {
+    //             return dp[i][j] = f(i + 1, j + 1, s, p, dp);
+    //         }
+    //         return false; // clear mismatch
+    //     }
+    // }
     bool isMatch(string s, string p) {
         n = s.size();
         m = p.size();
