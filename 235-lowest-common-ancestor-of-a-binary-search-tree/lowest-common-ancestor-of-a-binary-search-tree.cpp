8b4d2d82fce92713b5d80f3564c11401a7db1cf5
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-11 01:35:00 +0530

    Time: 25 ms (40.04%) | Memory: 23.5 MB (66.30%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,18 +10,24 @@
 
 class Solution {
 public:
+// jis pt pe do alg alg side jana pde , that same node is lca
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) return NULL;
 
-        int curr = root->val;
-        if(curr < p->val && curr < q->val){
-            return lowestCommonAncestor(root->right, p , q);
-        }
-        if(curr > p->val && curr > q->val){
-            return lowestCommonAncestor(root->left, p , q);
+        while(root){
+            if(root->val > p->val && root->val > q->val){
+                root = root->left;
+            }
+            else if(root->val < p->val && root->val < q->val){
+                root = root->right;
+            }
+            else{
+                return root;
+            }
         }
 
-        return root;
-        
+        return NULL;
+
+
     }
 };
