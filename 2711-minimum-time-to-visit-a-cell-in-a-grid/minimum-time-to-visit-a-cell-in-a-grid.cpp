252af44b47be41c5265d4404492d14c083d9ac7c
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-17 02:17:09 +0530

    Time: 573 ms (16.36%) | Memory: 159.2 MB (8.55%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -3,7 +3,7 @@
 //if we dont use vis array at all , we would be stuck in infinite recursion as a recursion cal go to, next cell and prev cell so like A-B and B-A  again n again.
 //hence think of smth else.
 
-// DJIKSTRA algo , we treat time to reach nodes as their edge wt and store them with gid cells nto the PQ. we know if somehow we leave grid[0][0] then we can def reach dest cell bcz then we would have one cell to go back and forth to waste time. we just need to make couple of obvs to calc time to reach nodes carefully is all. 
+// DJIKSTRA algo , we treat time to reach nodes as their edge wt and store them with grid cells into the PQ. we know if somehow we leave grid[0][0] then we can def reach dest cell bcz then we would have one cell to go back and forth to waste time. we just need to make couple of obvservation to calc time to reach nodes carefully. keep the algo exactly same , we try every nodes 
 class Solution {
 public:
     vector<vector<int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
@@ -20,24 +20,24 @@ public:
         vector<vector<int>> result(m, vector<int>(n, INT_MAX));  //dist 2D vector but instead of dist it cnts time
         priority_queue<P, vector<P>, greater<P>> pq;
         
-        pq.push({0, {0, 0}}); // Start at top-left corner
+        pq.push({0, {0, 0}}); // {t , {i,j}}
         result[0][0] = 0;
         while (!pq.empty()) {
-            // Get the current time, row, and column
             int time = pq.top().first;
             int row  = pq.top().second.first;
             int col  = pq.top().second.second;
             pq.pop();
             
-            // Reached destination
-            if (row == m - 1 && col == n - 1)
+        
+            if (row == m - 1 && col == n - 1){
                 return result[m-1][n-1];
+            }
+                
+            if (visited[row][col]) continue; // Mark the current cell as visited
+
+            visited[row][col] = true; //we mark visited only when we get this node from pq ie it means this is minimum possible time to reach this node , no lesser time can be found , thats why visited is imp here bcz it essentially tells us , if this is the min time to reach this node, why would we reach here again to only inc time
             
-            // Mark the current cell as visited
-            if (visited[row][col]) continue;
-            visited[row][col] = true;
-            
-            // Explore the neighboring cells
+//remember even tho we mark the prev as visited, in our head for time wasting we would need to go back and forth in one prev cell.
             for (auto d: dir) {
                 int r = row + d[0];
                 int c = col + d[1];
@@ -47,15 +47,17 @@ public:
                 if (grid[r][c] <= time+1) {
                     pq.push({time+1, {r, c}});
                     result[r][c] = time+1;
+                    
                 } else if ((grid[r][c]-time)%2==0) {
                     pq.push({grid[r][c]+1, {r, c}});
-                    result[r][c] = grid[r][c]+1;
+                    result[r][c] = grid[r][c]+1;  //if their diff is even ,then we can go back n forth to waste time and time will get rime + 1 when we reach that cell
                 } else {
                     pq.push({grid[r][c], {r, c}});
-                    result[r][c] = grid[r][c];
+                    result[r][c] = grid[r][c];  //if diff is odd , then after wasting time , we will exactly reach at time = grid[r][c] at (r,c).
                 }
             }
         }
         return -1;
     }
 };
+//we dont need result arr , we can do this without it too.
