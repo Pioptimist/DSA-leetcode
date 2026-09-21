1592cc73507575136cf9f3b41b3866a50c894900
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-21 13:07:16 +0530

    Time: 11 ms (12.25%) | Memory: 23.9 MB (33.56%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,10 +3,12 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(begin(intervals),end(intervals));
         int n = intervals.size();
+
         vector<vector<int>> mergeInt;
+        
         if(n==0) return mergeInt;
-        vector<int> tempInt = intervals[0];
-        for(auto it : intervals){
+       
+        for(const auto& it : intervals){
             if(mergeInt.size() ==0 || mergeInt.back()[1] < it[0]){
                 //start a new interval
                 mergeInt.push_back(it);
@@ -15,7 +17,7 @@ public:
                 mergeInt.back()[1] = max(mergeInt.back()[1] , it[1]); //update the interval if the curr interval start lies inside the last interval's end and then update the end.
             }
         }
-        // mergeInt.push_back(tempInt);
+        
         return mergeInt;
         
     }
