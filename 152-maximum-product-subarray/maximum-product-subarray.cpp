e2d91c02c98d244ab24e82fd2ef1c418e466386d
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-21 13:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (59.98%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,11 +5,15 @@ public:
         int pref = 1;
         int suff = 1;
         int maxi = INT_MIN;
+
         for(int i =0;i<n;i++){
+
             if(pref==0) pref=1;
             if(suff==0) suff=1;
+
             pref = pref*nums[i];
             suff= suff*nums[n-i-1];
+            
             maxi = max(maxi,max(pref,suff));
         }
         return maxi;
