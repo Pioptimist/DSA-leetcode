84584bb0359d5f325b04bcc0216cdc6fd986f76b
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-23 13:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (37.56%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// we can apply BS on the correct sorted half , so first we need to find the sorted half and then search in that half
     int search(vector<int>& nums, int target) {
         int low = 0;
         int n = nums.size();
@@ -7,7 +8,9 @@ public:
         while(low<=high){
             int mid =(low+high)/2;
             if(nums[mid]==target)  return mid;
-            else if(nums[low]<=nums[mid]){
+
+            else if(nums[low]<=nums[mid]){  //left half is sorted
+
                 if(target<=nums[mid] && target>=nums[low]){
                     high = mid-1;
                 }
