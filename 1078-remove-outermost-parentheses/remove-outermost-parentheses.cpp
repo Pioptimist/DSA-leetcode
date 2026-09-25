1aa425686fec6cd4850df6739fff2155a98c46b6
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-25 19:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (53.81%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+//remove outermost parenthesis of every balanced parenthesis ie (()) is a balanced one , 
+// (()()) is balanced
     string removeOuterParentheses(string s) {
        int n = s.size();
        string result = "";
        int cnt =0;
        for(int i =0;i<n;i++){
+        
         if(s[i]==')') cnt--;
+
         if(cnt!=0) result.push_back(s[i]);
+
         if(s[i] == '(') cnt++;
        } 
        return result;
