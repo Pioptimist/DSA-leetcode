209b3430f9f4077940261ca000d647d5e50cfd19
Soumyodeep Chakraborty <166546490+Pioptimist@users.noreply.github.com>
2026-09-25 13:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (78.15%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+// since the given properties makes the whole matrix as sorted so we can apply bs as we do on 1D
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int l = 0;
         int n = matrix.size();
         int m = matrix[0].size();
         int h = (n*m)-1;
+        
         while(l<=h){
             int mid =(l+h)/2;
+            // find row and col from this m
             int r = mid/m;
             int c = mid%m;
             if(matrix[r][c]==target) return true;
+
             else if(matrix[r][c]>target) h = mid-1;
+
             else l = mid+1;
         }
         return false;
